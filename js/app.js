e494de7ec3a20dbc7306b3ae340b41ce204df089
Akiraz <juanzap14@hotmail.com>
2023-06-23 18:10:45 -0500

    tabs in cards

--- js/app.js
+++ js/app.js
@@ -1,12 +1,41 @@
 import data from '../data/pokedb.json' assert { type: 'json' };
 
+const tabsContainer = document.querySelector(".tabs");
+var types = ['All'];
+
+data.cards.forEach((result) => {
+    for (let i=0; i<result.type.length;i++) {
+        if(!types.includes(result.type[i])) {
+            types.push(result.type[i]);
+        }
+    }
+});
+
+types.forEach(element => {
+  if (element == 'All') {
+    const tabsContent = `
+        <li class="active" data-tabs="${element}">${element}</li>
+    `;
+    tabsContainer.innerHTML += tabsContent;
+  } else {
+    const tabsContent = `
+        <li data-tabs="${element}">${element}</li>
+    `;
+    tabsContainer.innerHTML += tabsContent;
+  }
+});
+
 const container = document.getElementById('cards_shop');
 
 data.cards.forEach((result, idx) => {
 
-    // Construct card content
+    var pokeTypes = '';
+    result.type.forEach(typeName => {
+        pokeTypes += (pokeTypes != '' ? ' ' : '') + typeName;
+    });
+    
     const content = `
-    <div class="card">
+    <div class="card ${pokeTypes}">
         <div class="card_header" id="heading-${idx}">
             <p><b>${result.name}</b></p>
             <button class="simple__btn heart__btn"><i class="fa-regular fa-heart fa-xl"></i></button>
@@ -20,7 +49,38 @@ data.cards.forEach((result, idx) => {
         </div>
     </div>
     `;
-    // Append newyly created card element to the container
     container.innerHTML += content;
 
+});
+
+var tabs = document.querySelectorAll(".tabs li");
+var all = document.querySelectorAll(".card");
+
+tabs.forEach((tab)=>{
+  tab.addEventListener("click", ()=>{
+    tabs.forEach((tab)=>{
+      tab.classList.remove("active");
+    })
+    tab.classList.add("active");
+    var tabval = tab.getAttribute("data-tabs");
+
+    if (tabval == "All") {
+      all.forEach((item)=>{
+        item.style.display = "block";
+      })
+    } else {
+      all.forEach((item)=>{
+        item.style.display = "none";
+      });
+    }
+    
+    var typeCards = document.querySelectorAll("."+tabval);
+
+    typeCards.forEach((card) => {
+      card.style.display = "block";
+    });
+  })
 })
+
+
+
